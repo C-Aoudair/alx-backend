84dd2f1bc93ed43c6f87f68b0216dfeade75d2c7
C-Aoudair <aoudair16031999@gmail.com>
2024-06-26 17:27:24 +0100

    add task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Contains MRUCache class that inherites from BaseCaching.
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Implement caching system with MRU eviction policy."""
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Add item to self.cache_data dictionary."""
+        if not key or not item:
+            return
+
+        if key in self.cache_data:
+            self.cache_data.pop(key)
+
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_key = next(reversed(self.cache_data))
+            print(f"DISCARD: {last_key}")
+            self.cache_data.pop(last_key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key."""
+        value = self.cache_data.pop(key, None)
+        if value is not None:
+            self.cache_data[key] = value
+        return value

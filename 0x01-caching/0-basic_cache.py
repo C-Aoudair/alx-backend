b05f05816f637f27a52c0d48479a6b98d542ef6e
C-Aoudair <aoudair16031999@gmail.com>
2024-06-26 13:17:30 +0100

    add task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Contains BasicCache Class that inherits from BaseCaching class.
+"""
+
+from BaseCaching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ a simple caching system"""
+    def put(self, key, item):
+        """assing a key item pair to the self.cache_data dictionary"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        return self.cache_data.get(key)

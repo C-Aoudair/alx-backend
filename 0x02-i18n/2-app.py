b5f7abcf2ca605cb0e401f4a30fa018a59db46ec
C-Aoudair <aoudair16031999@gmail.com>
2024-07-03 18:15:30 +0100

    add 2-app.py and templates/2-index.html files

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,10 +19,16 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    # Determines the best match with our supported languages
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', strict_slashes=False)
 def index():
     """return index.html page"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':

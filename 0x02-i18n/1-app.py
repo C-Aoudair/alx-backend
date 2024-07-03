ef90703bcf085d7926a553b14f74f46451a3512c
C-Aoudair <aoudair16031999@gmail.com>
2024-07-03 17:11:13 +0100

    add 1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" a simple flask app"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config:
+    """ Config Class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """return index.html page"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()

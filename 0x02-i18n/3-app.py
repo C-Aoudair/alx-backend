38b4eb4d292bf77c0f4150973661a055c6ce9f44
C-Aoudair <aoudair16031999@gmail.com>
2024-07-06 14:28:15 +0100

    test

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,8 +4,6 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
 
-app = Flask(__name__)
-
 
 class Config:
     """ Config Class"""
@@ -14,21 +12,26 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
-
-
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """Determines the best match with our supported languages"""
+def get_locale() -> str:
+    """Determines the best match with our supported languages
+    Returns:
+        stre: best match
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """return index.html page"""
+    """render index.html page
+    Returns:
+        html tmeplate 3-index.html
+    """
     return render_template('3-index.html', get_locale=get_locale)
 
 

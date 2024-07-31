4d29430a55e168f0d7d84d9974263a0b39a42db0
C-Aoudair <aoudair16031999@gmail.com>
2024-07-31 03:19:31 +0100

    add files: 6-job_creator.js, 6-job_processor.js

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,13 @@
+import kue from 'kue';
+
+const push_notifiction_code = kue.createQueue();
+const data = {
+    phoneNumber: '4153518780',
+    message: 'This is the code to verify your account'
+};
+
+const job = push_notifiction_code.create('push_notification_code', data).save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
+});
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', () => console.log('Notification job failed'));
